75f8e6005e5c7fb1f3240ddb3b7fb2fc591fdf45
Nihanshu Purohit <nihanshupurohit@gmail.com>
2017-10-30 22:09:40 -0400

    Script modified and tested. Modified Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # kiki-mobileApp
+
+
+
+Get the python bluez service:
+
+"sudo apt-get install bluetooth libbluetooth-dev"
+"sudo python3 -m pip install pybluez"
+
+edit some dbus-org.bluez.service file and add -C flag in the ExecParam
+
+Get the python wifi service:
+"sudo python3 -m pip install wifi"
+
+To make the script exceutable & run: 
+
+"chmod +x run.py" 
+"sudo python2 run.py"
--- run.py
+++ run.py
@@ -16,13 +16,14 @@ sudo_mode = "sudo "
 def wifi_connect(ssid, psk):
     # write wifi config to file
     f = open('wifi.conf', 'w')
-    f.write('country=US\n')
+    #f.write('country=US\n')
     f.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
     f.write('update_config=1\n')
     f.write('\n')
     f.write('network={\n')
     f.write('    ssid="' + ssid + '"\n')
     f.write('    psk="' + psk + '"\n')
+    f.write('    key_mgmt=WPA-PSK\n')
     f.write('}\n')
     f.close()
 
@@ -31,49 +32,18 @@ def wifi_connect(ssid, psk):
     cmd_result = os.system(cmd)
     print(cmd + " - " + str(cmd_result))
 
-
-    # restart wifi adapter
-    cmd = sudo_mode + 'ifdown wlan0'
+    cmd = 'wpa_cli -i wlan0 reconfigure'
+    cmd_result = ""
     cmd_result = os.system(cmd)
     print(cmd + " - " + str(cmd_result))
-
-    time.sleep(2)
-
-    cmd = sudo_mode + 'ifup wlan0'
-    cmd_result = os.system(cmd)
-    print(cmd + " - " + str(cmd_result))
-
-    time.sleep(10)
-
-    cmd = 'iwconfig wlan0'
-    cmd_result = os.system(cmd)
-    print(cmd + " - " + str(cmd_result))
-
-    cmd = 'ifconfig wlan0'
-    cmd_result = os.system(cmd)
-    print(cmd + " - " + str(cmd_result))
-
-    p = subprocess.Popen(['ifconfig', 'wlan0'], stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-
-    out, err = p.communicate()
-
-    ip_address = "<Not Set>"
-
-    for l in out.split('\n'):
-        if l.strip().startswith("inet addr:"):
-            ip_address = l.strip().split(' ')[1].split(':')[1]
-
-    return ip_address
-
-
+ 
 
 def ssid_discovered():
     Cells = Cell.all('wlan0')
 
     wifi_info = 'Found ssid : \n'
 
-    for current in range(len(Cells)):
+    for current in range(len(list(Cells))):
         wifi_info +=  Cells[current].ssid + "\n"
 
 
@@ -85,37 +55,33 @@ def ssid_discovered():
 
 def handle_client(client_sock) :
     # get ssid
-    client_sock.send(ssid_discovered())
-    print("Waiting for SSID...")
+   client_sock.send(ssid_discovered())
+   print("Waiting for SSID...")
 
 
-    ssid = client_sock.recv(1024)
-    if ssid == '' :
-        return
+   ssid = client_sock.recv(1024)
+   if ssid == '' :
+       return
 
-    print("ssid received")
-    print(ssid)
+   print("ssid received")
+   print(ssid)
 
     # get psk
-    client_sock.send("waiting-psk!")
-    print("Waiting for PSK...")
+   client_sock.send("waiting-psk!")
+   print("Waiting for PSK...")
 
 
-    psk = client_sock.recv(1024)
-    if psk == '' :
-        return
+   psk = client_sock.recv(1024)
+   if psk == '' :
+       return
 
-    print("psk received")
+   print("psk received")
 
-    print(psk)
+   print(psk)
 
-    ip_address = wifi_connect(ssid, psk)
+   wifi_connect(ssid,psk)
 
-    print("ip address: " + ip_address)
-
-    client_sock.send("ip-addres:" + ip_address + "!")
-
-    return
+   return
 
 
 
